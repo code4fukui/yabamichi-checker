2e6af552840551760a0cd7c0a534706e9548c8fb
ninja03 <10812759-ninja03@users.noreply.replit.com>
2023-03-02 12:39:28 +0000

    a

--- main.tsx
+++ main.tsx
@@ -1,17 +1,43 @@
 /** @jsx h */
 import { h, jsx, json, serve, serveStatic } from "./deps.ts";
-import { routeAPI } from "./route_api.ts";
 import { Index } from "./index.tsx";
 import { Select } from "./select.tsx";
 import { Result } from "./result.tsx";
+import { Point } from "./point.ts";
+
+const result = async (req: Request) => {
+  const { searchParams } = new URL(req.url)
+  const from = new Point(
+    Number(searchParams.get("from_lat")),
+    Number(searchParams.get("from_lng")),
+  );
+  const to = new Point(
+    Number(searchParams.get("to_lat")),
+    Number(searchParams.get("to_lng")),
+  );
+  const line = await from.to(to).searchRoute();
+
+    // 危険地帯表示
+  const set = new Set();
+  const dangerSpots: any[] = [];
+  for (const pos of line) {
+    const point = new Point(pos.lat, pos.lng);
+    const spots = await point.searchSpot();
+    for (const spot of spots) {
+      const key = JSON.stringify(spot);
+      if (set.has(key)) {
+        continue;
+      }
+      set.add(key);
+      dangerSpots.push(spot);
+    }
+  }
+  return jsx(<Result line={line} dangerSpots={dangerSpots} />);
+}
 
 serve({
   "/": () => jsx(<Index/>),
   "/select": () => jsx(<Select/>),
-  "/result": () => jsx(<Result/>),
-  "/route": async (req) => {
-    const { searchParams } = new URL(req.url);
-    return json(await routeAPI(searchParams));
-  },
-  "/:filename+": serveStatic("public", { baseUrl: import.meta.url }),
+  "/result": async (req: Request) => await result(req),
+  "/:filename+": serveStatic("public", { baseUrl: import.meta.url })
 });
--- public/result.js
+++ public/result.js
@@ -1,30 +1,19 @@
 import { showMap } from "./map.js";
 
-const summary = document.getElementById("summary");
-const map = showMap("map");
-
-// パラメータ
-const param = new URLSearchParams(location.search);
-const fromLat = param.get("from_lat");
-const fromLng = param.get("from_lng");
-const toLat = param.get("to_lat");
-const toLng = param.get("to_lng");
-
-onload = async () => {
-  // ルート検索
-  const url =
-    `/route?from_lat=${fromLat}&from_lng=${fromLng}&to_lat=${toLat}&to_lng=${toLng}&`;
-  const resp = await fetch(url);
-  const route = await resp.json();
-
+onload = () => {
+  const mapE = document.getElementById("map");
+  const map = showMap("map");
   const opt = {
     "color": "#FF0000",
     "weight": 5,
     "opacity": 0.6,
   };
-  L.polyline(route.line, opt).addTo(map);
+  const line = JSON.parse(mapE.dataset.line);
+  L.polyline(line, opt).addTo(map);
 
   // 危険地帯表示
+  const dangerSpots = JSON.parse(mapE.dataset.dangerSpots);
+
   const bindOpt = {
     permanent: true,
     className: "my-label",
@@ -32,14 +21,12 @@ onload = async () => {
   };
 
   let i = 1;
-  for (const spot of route.dangerSpots) {
+  for (const spot of dangerSpots) {
     const txt = spot.txt.replaceAll("\n", "<br>");
     new L.Marker([spot.lat, spot.lng])
       .bindPopup(txt)
       .bindTooltip("" + i, bindOpt)
       .addTo(map);
-
-    summary.innerHTML += "<li>" + txt + "</li>";
     i++;
   }
 };
